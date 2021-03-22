a5d3ae64fc1a30544ef07e28e2c015cde2e4879e
angela-wheatley <79345893+angela-wheatley@users.noreply.github.com>
2021-03-22 18:50:49 -0500

    first commit of project

---
+++ src/main/java/com/tts/springBootIntroHW/SpringBootIntroHwApplication.java
@@ -0,0 +1,13 @@
+package com.tts.springBootIntroHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntroHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/springBootIntroHW/SpringBootIntroHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.springBootIntroHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
